0c833c46f678b90e9eb3f4e14d3c2db9a76f360f
bigdreamsweb3 <italianloader@gmail.com>
2025-10-24 23:22:47 +0100

    Something just got updated!

--- lib/providers.tsx
+++ lib/providers.tsx
@@ -1,23 +1,21 @@
 'use client';
 import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core';
-import { SolanaWalletConnectors } from "@dynamic-labs/solana";
+import { SolanaWalletConnectors } from '@dynamic-labs/solana';
 import { useEffect, useState } from 'react';
 
 export default function DynamicAuthProvider({ children }: { children: React.ReactNode }) {
     const [mounted, setMounted] = useState(false);
     const environmentId = process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID;
-    
+
     useEffect(() => {
         setMounted(true);
     }, []);
-    
+
     if (!environmentId) {
         throw new Error('NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID is required');
     }
 
-    if (!mounted) {
-        return <div>{children}</div>;
-    }
+    if (!mounted) return null; // ✅ no rendering during SSR
 
     return (
         <DynamicContextProvider
