5895ab894f747f2ea7aed2658ce759d723a1bc96
bigdreamsweb3 <italianloader@gmail.com>
2025-10-25 00:03:54 +0100

    Something just got updated!

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
-  webpack: (config, { dev, isServer }) => {
+  webpack: (config, { dev }) => {
     if (dev) {
       config.watchOptions = {
         poll: 1000,
         aggregateTimeout: 300,
       }
     }
+
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
     }
+
     return config
   },
 }
