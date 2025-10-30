fbde30737b000f071c0cbcc8c5d881a3f31af6cb
Big Dreams Web3 - Crafts ◯/| <123673781+bigdreamsweb3@users.noreply.github.com>
2025-10-30 10:09:15 +0100

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-[max(0.25rem,0.5vw)] md:gap-2 whitespace-nowrap font-medium transition-all duration-300 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-[max(0.9rem,1.8vw)] [&_svg:not([class*='size-'])]:md:size-4 shrink-0 [&_svg]:shrink-0 outline-none rounded-xl backdrop-blur-sm relative overflow-hidden",
+  "inline-flex items-center justify-center gap-[max(0.25rem,0.5vw)] md:gap-2 whitespace-nowrap text-sm md:text-[15px] font-medium transition-all duration-300 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-[max(0.9rem,1.8vw)] [&_svg:not([class*='size-'])]:md:size-4 shrink-0 [&_svg]:shrink-0 outline-none rounded-xl backdrop-blur-sm relative overflow-hidden",
   {
     variants: {
       variant: {
