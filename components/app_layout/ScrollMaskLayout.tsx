1d01fd557d550fc1b54ab7275b2b599af5bb640e
Big Dreams Web3 - Crafts ◯/| <123673781+bigdreamsweb3@users.noreply.github.com>
2025-11-08 09:54:55 +0100

    Update ScrollMaskLayout.tsx

--- components/app_layout/ScrollMaskLayout.tsx
+++ components/app_layout/ScrollMaskLayout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 interface ScrollMaskLayoutProps {
   children: ReactNode;
-  topMaskHeight?: number; // height in px to hide at the top
+  topMaskHeight?: number;
 }
 
 export default function ScrollMaskLayout({
@@ -12,11 +12,14 @@ export default function ScrollMaskLayout({
   topMaskHeight = 56,
 }: ScrollMaskLayoutProps) {
   return (
-    <div className="relative w-full h-full overflow-hidden">
+    <div className="relative w-full h-full">
       {/* Scrollable area */}
       <div
         className="overflow-y-auto w-full h-full"
-        style={{ paddingTop: topMaskHeight }}
+        style={{
+          paddingTop: topMaskHeight,
+          WebkitOverflowScrolling: "touch", // smooth scrolling on iOS
+        }}
       >
         {children}
       </div>
@@ -26,8 +29,8 @@ export default function ScrollMaskLayout({
         className="absolute top-0 left-0 right-0"
         style={{
           height: topMaskHeight,
-          background: "inherit", // match background
-          pointerEvents: "none", // allows scrolling through
+          background: "inherit",
+          pointerEvents: "none",
         }}
       />
     </div>
