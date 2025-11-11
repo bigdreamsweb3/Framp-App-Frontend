b320d4d6f1c1530e0dd02b13817152e23975a4b7
Big Dreams Web3 - Crafts ◯/| <123673781+bigdreamsweb3@users.noreply.github.com>
2025-11-12 00:08:34 +0100

    Update ScrollMaskLayout.tsx

--- components/app_layout/ScrollMaskLayout.tsx
+++ components/app_layout/ScrollMaskLayout.tsx
@@ -12,7 +12,7 @@ export default function ScrollMaskLayout({
   topMaskHeight = 56,
 }: ScrollMaskLayoutProps) {
   return (
-    <div className="relative w-full h-full border-t border-primary/10 with-bg-img">
+    <div className="relative w-full h-full border-t with-bg-img">
       {/* Scrollable area */}
       <div
         className="overflow-y-auto w-full h-full"
